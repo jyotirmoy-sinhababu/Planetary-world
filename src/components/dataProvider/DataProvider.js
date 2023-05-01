52cfc6ffc6f6100c1609fcb54618272060fa5439
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-01 23:12:40 +0530

    search functionality added to nav bar and use context hook is used to store global state

--- src/components/dataProvider/DataProvider.js
+++ src/components/dataProvider/DataProvider.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 import { createContext } from 'react';
 import { useState } from 'react';
 
-const ApiDataContext = createContext();
+export const ApiDataContext = createContext();
 
 const ApiProvider = ({ children }) => {
   const [planetaryData, setPlanetaryData] = useState();
-  const [searchInput, setSearchInput] = useState();
+  const [searchInput, setSearchInput] = useState('');
 
   const fetchData = async () => {
     const options = {
       method: 'GET',
       url: 'https://planets-by-api-ninjas.p.rapidapi.com/v1/planets',
-      params: { name: 'Mars' },
+      params: { name: `${searchInput.planetName}` },
       headers: {
         'X-RapidAPI-Key': 'ad1a068f96msh324f7c4f3d2d637p177632jsn800d1f15a6b6',
         'X-RapidAPI-Host': 'planets-by-api-ninjas.p.rapidapi.com',
@@ -22,13 +22,17 @@ const ApiProvider = ({ children }) => {
     try {
       await axios.request(options).then((res) => {
         setPlanetaryData(res.data);
+        console.log('called');
+        console.log(res.data);
       });
     } catch (err) {
       console.log('error');
     }
   };
   return (
-    <ApiDataContext.Provider value={{ planetaryData, setSearchInput }}>
+    <ApiDataContext.Provider
+      value={{ searchInput, planetaryData, setSearchInput, fetchData }}
+    >
       {children}
     </ApiDataContext.Provider>
   );
