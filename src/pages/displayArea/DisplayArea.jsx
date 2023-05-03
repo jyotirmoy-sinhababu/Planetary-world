d950cd3e22b95826bf3ed6c2d145d6b0fa1ebed0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-03 13:22:25 +0530

    data displayed

--- src/pages/displayArea/DisplayArea.jsx
+++ src/pages/displayArea/DisplayArea.jsx
@@ -16,24 +16,29 @@ const DisplayArea = () => {
 
   return (
     <div className='display-main-cnt'>
-      <div>
+      <div className='display-data-cnt'>
         {' '}
         {planetaryData.length ? (
           <div className='display-information-cnt'>
             <div>
               <div>
-                <h1>{planetaryData[0].name}</h1>
+                <h1 className='data'>{planetaryData[0].name}</h1>
+              </div>
+              <div>
+                <p className='data'>
+                  {planetaryData[0].mass * ((1.898 * 10) ^ 27)}kg
+                </p>
               </div>
             </div>
           </div>
         ) : (
-          <div>
-            <h1>No data found</h1>
+          <div className='display-err-cnt'>
+            <h1 className='display-err-msg'>No data found</h1>
           </div>
         )}
       </div>
 
-      <div className='planet-cnt'>
+      <div className='display-planet-cnt'>
         <PlanetsMovement />
       </div>
     </div>
